7386ec0cebe570ada8531547333e7533fed551f3
Roslyn Saplicki <j.saplicki@gmail.com>
2018-12-05 14:19:42 -0800

    Updates

---
+++ precache-manifest.9d557387945bbac8c89db70a90315bef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee902e61a5a89ae0d65b",
+    "url": "/project-4-assessment/static/js/runtime~main.ee902e61.js"
+  },
+  {
+    "revision": "b51c15ef452405697e91",
+    "url": "/project-4-assessment/static/js/main.b51c15ef.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/project-4-assessment/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "b51c15ef452405697e91",
+    "url": "/project-4-assessment/static/css/main.39a3000e.chunk.css"
+  },
+  {
+    "revision": "5e860778e863b3d709c1fd2ee0d222d5",
+    "url": "/project-4-assessment/index.html"
+  }
+];
